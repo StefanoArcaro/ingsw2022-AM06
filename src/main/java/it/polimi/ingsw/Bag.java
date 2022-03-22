9b21d2be71f973a04036f15874cf480cd0e411a2
nicholasbeltramini <nicholas.beltramini@mail.polimi.it>
2022-03-22 12:17:50 +0100

    added game manager and updated bag

---
+++ src/main/java/it/polimi/ingsw/Bag.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw;
+import java.util.ArrayList;
+
+public class Bag {
+    private static Bag bag = null;
+    private ArrayList<Student> students;
+
+    private Bag(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public static Bag getBag(){
+        if(bag == null){
+            bag = new Bag(null);
+        }
+        return bag;
+    }
+
+    public void addStudent(Student student){}
+    public Student drawStudent(){
+        return null;
+    }
+    public boolean isEmpty(){
+        return false;
+    }
+    public void Shuffle(){}
+}
