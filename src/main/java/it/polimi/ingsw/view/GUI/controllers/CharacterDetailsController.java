57cdd03da477df69c31de236dbcab1d41388f91f
nicholasbeltramini <nicholas.beltramini@mail.polimi.it>
2022-06-09 18:41:19 +0200

    finished characterDetails.fxml

---
+++ src/main/java/it/polimi/ingsw/view/GUI/controllers/CharacterDetailsController.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.view.GUI.controllers;
+
+import javafx.event.ActionEvent;
+
+public class CharacterDetailsController {
+    public void onSelectStudent(ActionEvent event) {
+    }
+
+    public void onSelectColor(ActionEvent event) {
+    }
+
+    public void onPlayCharacter(ActionEvent event) {
+    }
+
+    public void onGoBack(ActionEvent event) {
+    }
+}
